59bffd75d52960038f5ae064a9f4ae8a7f1f906f
Edvard <72183412+EdvardSoe@users.noreply.github.com>
2025-03-26 23:21:09 -0400

    API filled with jokes and punclines

---
+++ index.js
@@ -0,0 +1,66 @@
+
+// Declaring 'app', which represents the API. 
+// Express is declared as middleware
+const express = require ('express');
+const app = express();
+const PORT = 8080;
+
+
+
+
+// All info passed through the API goes 
+// through this middleware and is converted to json
+app.use(express.json() )
+
+
+// function to call the API
+app.listen(
+    PORT,
+    // 'callback' to let me know when the API is ready
+    () => console.log(`yo mama on ${PORT}`)
+)
+
+const jokes = [
+    { id: 1, joke: "Why did the scarecrow win an award?" },
+    { id: 2, joke: "What's the most expensive haircut?" },
+    { id: 3, joke: "What part of the bread factory do lobsters work in?" },
+    { id: 4, joke: "Where do lizards go when their tails fall off?" },
+    { id: 5, joke: "A dog walks into a pet store, mad as fuck. What does he say?" },
+    { id: 6, joke: "What happened to the cat who ate a ball of yarn?" },
+  ];
+
+  const answers = [
+    { id: 1, joke: "Because he was outstanding in his field!" },
+    { id: 2, joke: "Chemotherapy." },
+    { id: 3, joke: "The crust station." },
+    { id: 4, joke: "The retail store." },
+    { id: 5, joke: "I got a bone to pick." },
+    { id: 6, joke: "She had mittens." },
+  ];
+
+// Adding a get endpoint for the API. Whenever a user requests the URL, 
+// it calls this callback function. (Request/Response)
+app.get('/joke', (req, res) => {
+    res.status(200).send({
+        joke: jokes,
+        punchline: answers
+    })
+});
+
+
+// Seperate endpoint that captures dynamic values from the URL
+app.post('/joke/:id', (req, res) => {
+
+    const {id} = req.params;
+
+
+// Exception filter
+ //   if (!size){
+ //       res.status(418).send ({message: 'We need a size!'})
+ //   }
+
+    res.send({
+        joke: jokes,
+        punchline: answers
+    })
+});
